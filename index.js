2696fceb6abf67aacb380fa0d907f1781e2b065f
aneeqalouw <aneeqalouw057@gmail.com>
2024-02-07 11:15:01 +0200

    api function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Revision');
+//importing the functions
+const {addition} = require('./addition')
+const {subtraction} = require('./subtraction')
+
+console.log(addition(3,6));
+console.log(subtraction(7,3));
